acd4405b8f6fae91161f49d8677e002027e561fd
SaiNour <saidinourelhouda61@gmail.com>
2020-05-12 13:42:23 +0200

    modifiée code css de forgot pass

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
-
 @section('content')
+
 <div class="container" style="margin-top:10%;">
     <div class="row justify-content-center" >
         <div class="col-md-8">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -688,7 +688,7 @@
                 <ul class="header-cart-wrapitem w-full">
                     <li class="header-cart-item flex-w flex-t m-b-12">
                         <div class="header-cart-item-img">
-                            <img  src="{{ asset('images/item-cart-01.jpg')}}"  alt="IMG">
+                            <img  src="images/item-cart-01.jpg"  alt="IMG">
                         </div>
 
                         <div class="header-cart-item-txt p-t-8">
@@ -900,31 +900,31 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                                 <div class="wrap-slick3-arrows flex-sb-m flex-w"></div>
 
                                 <div class="slick3 gallery-lb">
-                                    <div class="item-slick3" data-thumb="{{ asset('images/product-detail-01.jpg')}}">
+                                    <div class="item-slick3" data-thumb="images/product-detail-01.jpg">
                                         <div class="wrap-pic-w pos-relative">
                                             <img src="{{ asset('images/product-detail-01.jpg')}}" alt="IMG-PRODUCT">
 
-                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{ asset('images/product-detail-01.jpg')}}">
+                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-01.jpg">
                                                 <i class="fa fa-expand"></i>
                                             </a>
                                         </div>
                                     </div>
 
-                                    <div class="item-slick3" data-thumb="{{ asset('images/product-detail-02.jpg')}}">
+                                    <div class="item-slick3" data-thumb="images/product-detail-02.jpg">
                                         <div class="wrap-pic-w pos-relative">
                                             <img src="{{ asset('images/product-detail-02.jpg')}}" alt="IMG-PRODUCT">
 
-                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{ asset('images/product-detail-02.jpg')}}">
+                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-02.jpg">
                                                 <i class="fa fa-expand"></i>
                                             </a>
                                         </div>
                                     </div>
 
-                                    <div class="item-slick3" data-thumb="{{ asset('images/product-detail-03.jpg')}}">
+                                    <div class="item-slick3" data-thumb="images/product-detail-03.jpg">
                                         <div class="wrap-pic-w pos-relative">
                                             <img src="{{ asset('images/product-detail-03.jpg')}}" alt="IMG-PRODUCT">
 
-                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="{{ asset('images/product-detail-03.jpg')}}">
+                                            <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-03.jpg">
                                                 <i class="fa fa-expand"></i>
                                             </a>
                                         </div>
