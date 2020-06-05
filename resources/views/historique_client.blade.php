a7185252bef6f6290025d470a26fd1778bd89077
SaiNour <saidinourelhouda61@gmail.com>
2020-06-05 16:09:54 +0200

    afficher_supprimer_historique_client

--- resources/views/historique_client.blade.php
+++ resources/views/historique_client.blade.php
@@ -1,184 +1,353 @@
 @extends('layouts.template_clinet')
+ 
 @section('content')
 
-	
-	<head>
-		<title>{{ ( 'Historique') }}</title>
-	</head>
-	<nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand" href="#pablo">Historique </a>
-          </div>
-        </div>
-    </nav>
-    <div class="panel-header panel-header-sm">
-    </div>
+<head>
+      <title>{{ ( 'Historique ') }}</title>
+  </head>
 
-
-	<div class="content" id="pr">
-         <div class="row"  >
-            <div class="col-md-12" >
-              <div class="card"  id="xc" style=" width: 1000px;"  >
-                <div class="card-header">
-                  <h5 class="titre" > 6 Avril 2020</h5>
-                </div>
+<div class="main-panel" id="main-panel">
+  
+  <div class="panel-header panel-header-sm" >
+  </div>
+  <div class="content" id="app">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header" >
                 
-              <div class="card-body">
-                <div class="table-responsive">
-                  <table class="table">
-                    <thead class=" text-primary">
-                     
-                    </thead>
-                    <tbody>
-                    <!--historique 1-->
-                         <tr>
-                 <td>
-   
-
-<form>
-  	<input type="checkbox"  name="subscribe" value="newsletter">  
-    <label for="subscribeNews" class="description text-right"  > 
-        <a class="dropdown-item " href="#" id="h" >21:48</a>
-    </label>
-    <label for="subscribeNews" >
-        <a class="dropdown-item"   href="#" id="d"  >   Vous Avez ajouter 'nom_Produit' sur votre panier</a>
-    </label>
-</form>
-
-  <td  class="dropdown " id="k3">
-    <a  data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#">
-         <img src="{{ asset('assetsClient/img/input/delete.png') }}" id="k1" height="20px" height="10px">
-     </a>
-    <div class="dropdown-menu " x-placement="right-start" id="supp">
-       <a class="dropdown-item" href="#" id="k3">Supprimer</a>
-    </div>
-  </td></tr>
-                        
-   
-
-                           <!--historique 2-->
-
-   
-                           <tr>
-                 <td>
-   
-
-<form>
-  	<input type="checkbox"  name="subscribe" value="newsletter">  
-    <label for="subscribeNews" class="description text-right"  > 
-        <a class="dropdown-item " href="#" id="h" >21:48</a>
-    </label>
-    <label for="subscribeNews" >
-        <a class="dropdown-item"   href="#" id="d"  >   Vous Avez ajouter 'nom_Produit' sur votre panier</a>
-    </label>
-</form>
-
-  <td  class="dropdown " id="k3">
-    <a  data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#">
-         <img src="{{ asset('assetsClient/img/input/delete.png') }}" id="k1" height="20px" height="10px">
-     </a>
-    <div class="dropdown-menu " x-placement="right-start" id="supp">
-       <a class="dropdown-item" href="#" id="k3">Supprimer</a>
-    </div>
-  </td></tr>
-                             <!--historique 3-->
-
-                             <tr>
-                 <td>
-   
-
-<form>
-  	<input type="checkbox"  name="subscribe" value="newsletter">  
-    <label for="subscribeNews" class="description text-right"  > 
-        <a class="dropdown-item " href="#" id="h" >21:48</a>
-    </label>
-    <label for="subscribeNews" >
-        <a class="dropdown-item"   href="#" id="d"  >   Vous Avez ajouter 'nom_Produit' sur votre panier</a>
-    </label>
-</form>
-
-  <td  class="dropdown " id="k3">
-    <a  data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#">
-         <img src="{{ asset('assetsClient/img/input/delete.png') }}" id="k1" height="20px" height="10px">
-     </a>
-    <div class="dropdown-menu " x-placement="right-start" id="supp">
-       <a class="dropdown-item" href="#" id="k3">Supprimer</a>
-    </div>
-  </td></tr>
-    <!--historique 4-->
-
-    <tr>
-                 <td>
-   
-
-<form>
-  	<input type="checkbox"  name="subscribe" value="newsletter">  
-    <label for="subscribeNews" class="description text-right"  > 
-        <a class="dropdown-item " href="#" id="h" >21:48</a>
-    </label>
-    <label for="subscribeNews" >
-        <a class="dropdown-item"   href="#" id="d"  >   Vous Avez ajouter 'nom_Produit' sur votre panier</a>
-    </label>
-</form>
-
-  <td  class="dropdown " id="k3">
-    <a  data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#">
-         <img src="{{ asset('assetsClient/img/input/delete.png') }}" id="k1" height="20px" height="10px">
-     </a>
-    <div class="dropdown-menu " x-placement="right-start" id="supp">
-       <a class="dropdown-item" href="#" id="k3">Supprimer</a>
-    </div>
-  </td></tr>
-              <!--historique 5-->
-
-              <tr>
-                 <td>
-   
-
-<form>
-  	<input type="checkbox"  name="subscribe" value="newsletter">  
-    <label for="subscribeNews" class="description text-right"  > 
-        <a class="dropdown-item " href="#" id="h" >21:48</a>
-    </label>
-    <label for="subscribeNews" >
-        <a class="dropdown-item"   href="#" id="d"  >   Vous Avez ajouter 'nom_Produit' sur votre panier</a>
-    </label>
-</form>
-
-  <td  class="dropdown " id="k3">
-    <a  data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#">
-         <img src="{{ asset('assetsClient/img/input/delete.png') }}" id="k1" height="20px" height="10px">
-     </a>
-    <div class="dropdown-menu " x-placement="right-start" id="supp">
-       <a class="dropdown-item" href="#" id="k3">Supprimer</a>
-    </div>
-  </td></tr>
-        </tbody>
-                  </table>
-                  <div class="pagination" >
-                        <a href="#"> &laquo; </a>
-                        <a href="#" class="active"> 1 </a>
-                        <a href="#"> 2 </a>
-                        <a href="#"> 3 </a>
-                        <a href="#"> 4 </a>
-                        <a href="#"> 5 </a>
-                        <a href="#"> &raquo; </a>
-
-
-                    </div>
+                <div class="flex-t">
+                    <input type="checkbox" id="article" @change="selectAll()" v-model="allSelected">
+                    <label for="article"></label>
+                    <h4 style="margin-top: -6px;">Historiques</h4>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+            <div class="txt-right"style="margin-top: -40px; " >
+                  <button v-if="suppr" class="btn-sm btn-danger " style="height: 35px; " v-on:click="deleteArrayArticle()"><b>Supprimer</b>
+                  </button>
+                  
+                  
+                  <button v-on:click="AnnulerSel()" v-if="suppr" class="btn-sm btn-warning " style="height: 35px; " ><b>Annuler</b>
+                  </button>
+               </div>
+         
+            
+            <hr style="margin-top:42px;">       
+          
         
+            <div class="card-body"   v-for="historiquec in historiqueclient" >
+
+<div v-if="selectall" >
+       <input type="checkbox" :id="historiquec.id" :value="historiquec.id" v-model="checkedArticles" @change="changeButton(historiquec)">
+      <label :for="historiquec.id" style="margin-top: 40px; margin-left: 10px;"></label>
+    </div>
+    <div v-else >
+      <div id="ch1">
+      <input type="checkbox" :id="historiquec.id" :value="historiquec.id" v-model="articleIds" @click="deselectArticle(historiquec.id)"></div>
+      <label :for="historiquec.id" style="margin-top: 40px; margin-left: 10px;"></label>
+    </div>
+
+
+  <div class="card-head"  id="hmd"  >              
+    <div class="row" >
+    <div >
+  <p class=""  id="hh" >
+       @{{historiquec.created_at}}</p>
+      </div> 
+    <div class="col-md-4 pr-1" v-if="historiquec.produit_id  === null" >
+      <div style="margin-left:22px" v-for="imgA in imagesannonce" v-if=" historiquec.annonceE_id  === imgA.id">
+          <p class="" id="h" > Vous Avez envoyez  annonce ' @{{imgA.libellé}}'</p>
+      </div>
+       
+    </div>
+    
+    <div  class="col-md-4 pr-1" v-else="historiquec.annonceE_id  === null" >
+      <div style="margin-left:22px" v-for="imgP in imagesproduit" v-if=" historiquec.produit_id  === imgP.id">
+          <p class="" id="h" >Vous Avez ajoutez  produit ' @{{imgP.Libellé}}' sur votre panier</p>
+      </div>
+       
+    </div>
+
+    <div class="col-md-4 pl-1" >
+      <div class="" >
+      <a class="f" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false" href="#" id="point">
+        <i class="fas fa-ellipsis-v"  id="y"></i>
+       </a>
+      <div class="dropdown-menu " x-placement="right-start" id="pl" >
+    <a class="dropdown-item" v-on:click="deleteHistorique(historiquec)"style="color: red; font-style: italic; font-weight: 900; cursor: pointer;">
+    Supprimer</a>
+       </div>
+      
+    </div>    
+
+    </div>      
+
+  </div>      
+
+  <hr>
+
+    
+</div>                   {{$article->links()}}
+              </div>
+
+            </div>      
+          </div>
+        </div>      
+      </div>
+    </div>
+
+  
+</div>
+  
+</div>
+
+
+
+  
+
+
+
+
+
 @endsection
+
+
+
+
+@push("javascripts")
+
+
+
+
+<script>
+    window.Laravel = {!! json_encode([
+           "csrfToken"  => csrf_token(),
+           "article"   => $article,
+           "idAdmin" => $idAdmin,
+           'ImageP'         => $ImageP,
+           'ImageA'         => $ImageA,
+           "url"      => url("/")  
+      ]) !!};
+</script>
+
+<script>
+
+
+
+var app2 = new Vue({
+  el: '#app2',
+  data:{
+    openInfo: false,
+
+    hideModel: false,
+   
+ 
+ 
+    detaillsA: {
+      idA: 0,
+    },
+
+  
+  
+               
+  },
+methods: {
+
+
+  
+
+
+  CancelArticle(article){
+    this.modifier = false ;
+    this.hideModel = false;
+    this.art = {        
+                  id: 0,
+                  admin_id: window.Laravel.idAdmin,
+                  titre: '', 
+                  description: '',
+                  image: ''
+    };
+    this.message = {};
+    article.titre = this.oldArt.titre;
+    article.description = this.oldArt.description;
+  },
+
+},    
+});
+
+var app = new Vue({
+
+el: '#app',
+
+
+data:{
+  historiqueclient: [],
+  imagesproduit:[],imagesannonce:[],
+  suppr: false, 
+  checkedArticles: [],
+  artilcesDelete: [],
+  allSelected: false,
+  articleIds: [],
+  selectall: true,
+
+  },
+methods: {
+  
+   deleteArrayArticle:function(){
+        if(this.artilcesDelete.length == 0){
+            Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Il ya aucun Historique a supprimer!',
+
+          }).then((result) => {
+            this.allSelected = false;
+            this.suppr=false;
+            this.selectall = true;
+           
+         })
+          return;
+        }
+        Swal.fire({
+        title: 'Etes vous?',
+        text: "De supprimer cette Historique?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Oui, Supprimer!'
+      }).then((result) => {
+          if (result.value) {
+            this.artilcesDelete.forEach(key => {
+              axios.delete(window.Laravel.url+'/deletehistorique/'+key.id)
+                .then(response => {
+                  if(response.data.etat){
+                                        
+                            var position = this.historiqueclient.indexOf(key);
+                            this.historiqueclient.splice(position,1);      
+                  }                    
+                })
+                .catch(error =>{
+                           console.log('errors :' , error);
+                })
+            })
+                this.allSelected = false;
+                this.checkedArticles.length = [];
+                this.suppr=false;
+                this.artilcesDelete = [];
+                this.selectall = true;
+          Swal.fire(
+            'Effacé!',
+            'Votre historique a été supprimé.',
+            'success'
+          )
+        }
+        
+        })
+   },
+ 
+   deleteHistorique: function(article){
+        Swal.fire({
+    title: 'Etes vous?',
+    text: "De supprimer cette Historique?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Oui, Supprimer!'
+  }).then((result) => {
+      if (result.value) {
+          axios.delete(window.Laravel.url+'/deletehistorique/'+article.id)
+            .then(response => {
+              if(response.data.etat){
+                       var position = this.historiqueclient.indexOf(article);
+                       this.historiqueclient.splice(position,1);
+                       this.checkedArticles.length = [];
+                       this.suppr=false;
+                       this.artilcesDelete = [];
+                       this.selectall = true;
+              }                     
+            })
+            .catch(error =>{
+                       console.log('errors :' , error);
+            })
+      Swal.fire(
+        'Effacé!',
+        'Votre historique a été supprimé.',
+        'success'
+      )
+    }
+    
+    })
+  },
+ 
+ 
+  
+  
+  
+  get_historique_client: function(){
+            axios.get(window.Laravel.url+'/historiqueClient')
+
+                .then(response => {
+                     this.historiqueclient = window.Laravel.article.data;
+                     this.imagesproduit = window.Laravel.ImageP;
+                     this.imagesannonce = window.Laravel.ImageA;
+                })
+                .catch(error =>{
+                     console.log('errors :' , error);
+                })
+  },
+  changeButton: function(a){
+    this.artilcesDelete.unshift(a);
+    if(this.checkedArticles.length > 0){
+      this.suppr=true;
+    }
+    else{
+      this.artilcesDelete = [];
+      this.suppr=false;
+    }        
+  }, 
+  AnnulerSel: function(){
+    this.checkedArticles.length = [];
+    this.changeButton();
+    this.selectall = true;
+    this.allSelected = false;
+  },
+ 
+ 
+  selectAll: function() {
+        this.selectall = false;
+        if (this.allSelected) {
+            for (user in this.historiqueclient) {
+                this.articleIds.push(this.historiqueclient[user].id);
+                this.artilcesDelete.push(this.historiqueclient[user]);
+            }
+            this.suppr=true;
+         }
+         else{
+          this.articleIds = [];
+          this.artilcesDelete= [];
+          this.suppr=false;
+          this.selectall = true;
+          this.checkedArticles = [];
+        }
+         
+    },
+    deselectArticle: function(article){
+         this.artilcesDelete.forEach(key => {
+              if(key.id == article){
+                  var position = this.artilcesDelete.indexOf(key);
+                  this.artilcesDelete.splice(position,1);                    
+              } 
+        });             
+    }
+},  
+created:function(){
+  this.get_historique_client();
+}
+});
+
+
+</script>
+
+@endpush
