49324554fccd926f2ac3ca13440354b170ab2a3c
SaiNour <saidinourelhouda61@gmail.com>
2020-06-14 20:57:06 +0000

    modifier favoris

--- app/Http/Controllers/HistoriqurController.php
+++ app/Http/Controllers/HistoriqurController.php
@@ -14,18 +14,17 @@ use Auth;
 
 class HistoriqurController extends Controller
 {
-    public function get_historique_client(){//fcnt qui retourné tout les articles qui sont dans la table "Article" et trie par ordre desc selon son dates de creations
-        $c = Client::find(Auth::user()->id);//recuperé "user_id" de admin qui est connecter       
-        $article = \DB::table('historiques')->where('client_id', $c->id)->orderBy('created_at','desc')->paginate(5) ;//recuperé les articles qui sont dans la table "Article" et trie par ordre desc selon son dates de creations et pour "->paginate(5)" c a d f kol page t'affichilek 5 ta3 les article  
-        $imageproduit = \DB::table('produits')->get(); 
-      
-        $imageannonce = \DB::table('annonce_emploies')->get();       
-        return view('historique_client',['article'=>$article, 'idAdmin' => $c->id,'ImageP' => $imageproduit,'ImageA' => $imageannonce]);//reteurné a la view "articles_admin" et les 2 attributs "article" (contient tout les articles) et "idAdmin" (id de l'admin cncté) 
+    public function get_historique_client(){
+        $c = Client::find(Auth::user()->id);
+        $article = \DB::table('historiques')->where('client_id', $c->id)->orderBy('created_at','desc')->paginate(10) ;
+        $produitss = \DB::table('produits')->get(); 
+        $annoncemploies = \DB::table('annonce_emploies')->get();       
+        return view('historique_client',['article'=>$article, 'idAdmin' => $c->id,'produitCL' => $produitss,'annoncemploiesCL' => $annoncemploies]);
     } 
     
-    public function deleteHistorique($id){//fnct pour supprimer un article di 3ando un parametre di hoda id ta3 article di nssuprimiwah w tretourni un attributs "etat"(ida kan = true => la supprision n3amlt ghaya)
-        $commande = Historique::find($id);//n7awes 3la l article di rena habin nsupprimiwah
-        $commande->delete();
+    public function deleteHistorique($id){
+        $historique = Historique::find($id);
+        $historique->delete();
         return Response()->json(['etat' => true]);
     }
 }
