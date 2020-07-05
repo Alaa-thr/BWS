6e2d168ef1b540c6343e6f9c864b558e9a70ebbb
SaiNour <saidinourelhouda61@gmail.com>
2020-07-05 15:36:21 +0000

    afficher les produit de panier

--- resources/views/favoris_client.blade.php
+++ resources/views/favoris_client.blade.php
@@ -1,3 +1,7 @@
+
+
+
+
 @extends('layouts.template_clinet')
 
 @section('content')
@@ -5,6 +9,61 @@
     <head>
         <title>{{ ( 'Favorie') }}</title>
     </head>
+     <!-- Cart -->
+     <div class="wrap-header-cart js-panel-cart" style="z-index: 11000; ">
+        <div class="s-full js-hide-cart"></div>
+        
+        <div class="header-cart flex-col-l p-l-55 p-r-25">
+            
+            <div class="header-cart-title flex-w flex-sb-m p-b-8">
+                <span class="mtext-103 cl2">
+                    Votre Panier
+                </span>
+
+                <div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart" >
+                    <i class="zmdi zmdi-close" style="margin-left: 171%"></i>
+                </div>
+                
+            </div>
+            
+            <div class="header-cart-content flex-w js-pscroll" id="app1" >
+                <ul class="header-cart-wrapitem w-full" v-for="command in ProduitsPanier">
+                    <li class="header-cart-item flex-w flex-t m-b-12" >
+                        <div class="header-cart-item-img" v-for="imgP in imagesproduit">
+                        <img v-if="imgP.produit_id === command.produit_id && imgP.profile === 1" :src="'storage/produits_image/'+ imgP.image" alt="IMG-PRODUCT" style="height: 60px;">
+                        </div>
+
+                        <div class="header-cart-item-txt p-t-8"  v-for="fv in favoris" v-if="fv.id === command.produit_id" >
+                            <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                            @{{fv.Libellé}}
+                            </a>
+
+                            <span class="header-cart-item-info">
+                            @{{command.qte}} x  @{{fv.prix}} DA
+                            </span>
+                        </div>
+                    </li>
+                </ul>
+                
+                <div class="w-full" >
+                    
+                <div class="header-cart-total w-full p-tb-40">
+                        Total: 
+                    </div>
+
+                    <div class="header-cart-buttons flex-w w-full">
+                        <a href="{{route('panier')}}" class="flex-c-m stext-101 cl0 size-107 bg10 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+                            View Cart
+                        </a>
+
+                        <a href="{{route('panier')}}" class="flex-c-m stext-101 cl0 size-107 bg10 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+                            Check Out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </div>
     <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
         <div class="container-fluid">
           <div class="navbar-wrapper">
@@ -88,9 +147,40 @@
                'produit'        => $produit,
                'ImageP'         => $ImageP,
                'Fav'         => $Fav,
-
+               'command'         => $command,
                 'url'           => url('/'), 
           ]) !!};
+</script>
+
+<script>
+     var app1 = new Vue({
+        el: '#app1',
+        data:{
+          message:'hello',
+          ProduitsPanier: [],
+          favoris: [],
+          imagesproduit: [],
+        },
+        methods:{
+          getProduit: function(){
+            axios.get(window.Laravel.url+'/favorisClient')
+              .then(response => {
+                this.favoris = window.Laravel.Fav;
+                this.imagesproduit = window.Laravel.ImageP;
+                this.ProduitsPanier = window.Laravel.command;
+               })
+              .catch(error => {
+                  console.log('errors : '  , error);
+             })
+          },
+          
+
+        },
+        created:function(){
+            this.getProduit();
+
+        }
+     })
 </script>
 
 <script>
@@ -103,6 +193,7 @@
           imagesproduit: [],
           favoris: [],
           p:[],
+          ProduitsPanier: [],
 
         },
         methods:{
@@ -112,6 +203,7 @@
                 this.ProduitsVendeur = window.Laravel.produit.data;
                 this.imagesproduit = window.Laravel.ImageP;
                 this.favoris = window.Laravel.Fav;
+                this.ProduitsPanier = window.Laravel.command;
                })
               .catch(error => {
                   console.log('errors : '  , error);
--- resources/views/notification_client.blade.php
+++ resources/views/notification_client.blade.php
@@ -6,6 +6,61 @@
       <title>{{ ( 'Notification ') }}</title>
   </head>
 
+  <!-- Cart -->
+  <div class="wrap-header-cart js-panel-cart" style="z-index: 11000; ">
+        <div class="s-full js-hide-cart"></div>
+        
+        <div class="header-cart flex-col-l p-l-55 p-r-25">
+            
+            <div class="header-cart-title flex-w flex-sb-m p-b-8">
+                <span class="mtext-103 cl2">
+                    Votre Panier
+                </span>
+
+                <div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart" >
+                    <i class="zmdi zmdi-close" style="margin-left: 171%"></i>
+                </div>
+                
+            </div>
+            
+            <div class="header-cart-content flex-w js-pscroll" id="app1" >
+                <ul class="header-cart-wrapitem w-full" v-for="command in ProduitsPanier">
+                    <li class="header-cart-item flex-w flex-t m-b-12" >
+                        <div class="header-cart-item-img" v-for="imgP in imagesproduit">
+                        <img v-if="imgP.produit_id === command.produit_id && imgP.profile === 1" :src="'storage/produits_image/'+ imgP.image" alt="IMG-PRODUCT" style="height: 60px;">
+                        </div>
+
+                        <div class="header-cart-item-txt p-t-8"  v-for="fv in favoris" v-if="fv.id === command.produit_id" >
+                            <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                            @{{fv.Libellé}}
+                            </a>
+
+                            <span class="header-cart-item-info">
+                            @{{command.qte}} x  @{{fv.prix}} DA
+                            </span>
+                        </div>
+                    </li>
+                </ul>
+                
+                <div class="w-full" >
+                    
+                <div class="header-cart-total w-full p-tb-40">
+                        Total: 
+                    </div>
+
+                    <div class="header-cart-buttons flex-w w-full">
+                        <a href="{{route('panier')}}" class="flex-c-m stext-101 cl0 size-107 bg10 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+                            View Cart
+                        </a>
+
+                        <a href="{{route('panier')}}" class="flex-c-m stext-101 cl0 size-107 bg10 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+                            Check Out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </div>
 <div class="main-panel" id="main-panel">
   
   <div class="panel-header panel-header-sm" >
@@ -127,7 +182,9 @@
            "idAdmin" => $idAdmin,
            'emploC'         => $emploC,
            'vendeurC'         => $vendeurC,
-
+               'ImageP'         => $ImageP,
+               'Fav'         => $Fav,
+               'command'         => $command,
            "url"      => url("/")  
       ]) !!};
 </script>
@@ -348,5 +405,34 @@ created:function(){
 
 
 </script>
+<script>
+     var app1 = new Vue({
+        el: '#app1',
+        data:{
+          message:'hello',
+          ProduitsPanier: [],
+          favoris: [],
+          imagesproduit: [],
+        },
+        methods:{
+          get_notification_client: function(){
+            axios.get(window.Laravel.url+'/notificationClient')
+              .then(response => {
+                this.favoris = window.Laravel.Fav;
+                this.imagesproduit = window.Laravel.ImageP;
+                this.ProduitsPanier = window.Laravel.command;
+               })
+              .catch(error => {
+                  console.log('errors : '  , error);
+             })
+          },
+          
 
+        },
+        created:function(){
+            this.get_notification_client();
+
+        }
+     })
+</script>
 @endpush
